4e3333f4156b4500f913699c7a38123a3c16f967
allen123 <allen.luan@mail.utoronto.ca>
2021-02-20 18:54:05 -0500

    Update on 0220

--- Project1/app/photo.py
+++ Project1/app/photo.py
@@ -4,5 +4,5 @@ from app import db
 class Photo(db.Model):
     __tablename__ = 'Photo'
     username = db.Column(db.String(100))
-    photourl=db.Column(db.String(100), primary_key=True)
+    photourl=db.Column(db.String(255), primary_key=True)
     imagetype = db.Column(db.Integer)
