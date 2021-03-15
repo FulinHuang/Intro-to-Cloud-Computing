7353e77c8d17f29ec3bd65b196d9061599de2c05
allen123 <allen.luan@mail.utoronto.ca>
2021-03-15 14:06:40 -0400

    Mar_15

--- Project2/app/config.py
+++ Project2/app/config.py
@@ -1,12 +1,26 @@
 import os
 
+############ALLEN#############
+image_id = 'ami-0a1a1906efbd4c711'
+key_pair = 'Allen_A2_User1'
+security_group = 'launch-wizard-1'
+worker_group = 'User_group_2'  # placement group for workers
+worker_group_2 = 'usergroup'  # placement group for workers
+manager_group = 'managergroup'  # placement group for managers
+
+elb_arn = 'arn:aws:elasticloadbalancing:us-east-1:011441637402:loadbalancer/app/allena2ELB/5360f782e6428bc4'
+elb_dns = 'allena2ELB-168220349.us-east-1.elb.amazonaws.com'
+elb_name = 'allena2ELB'
+target_group_arn = 'arn:aws:elasticloadbalancing:us-east-1:011441637402:targetgroup/allena2/c1b769c7b4575737'
+iam_arn = 'arn:aws:iam::011441637402:instance-profile/full_S3_access_from_EC2'
+################################
+
 # balancer
 arn = 'arn:aws:elasticloadbalancing:us-east-1:411033800461:loadbalancer/app/loadbalancer/36142359a6458967'
 dns = 'loadbalancer-1947271922.us-east-1.elb.amazonaws.com'
 target_arn = 'arn:aws:elasticloadbalancing:us-east-1:411033800461:targetgroup/1779lb/76c209c218e6fc76'
 target_group_dimension = 'targetgroup/allena2/c1b769c7b4575737'
 ELB_dimension = 'app/loadbalancer/3a7654beb0b62d6f'
-
 
 # Variable for creating an instance
 ImageId ='ami-03b4f4239f819d507'
@@ -18,7 +32,7 @@ tag_specificatios = [{
             'ResourceType': 'instance',
             'Tags': [{'Key': 'Name','Value': user_tag}]}]
 placement = {'AvailabilityZone': 'us-east-1a', 'GroupName': user_placement}
-security_group = 'launch-wizard-7'
+security_group_Irene = 'launch-wizard-7'
 iam_instance_profile = {'Arn': 'arn:aws:iam::411033800461:instance-profile/full_s3_access_from_ec2'}
 user_data = '''Content-Type: multipart/mixed; boundary="//"
 MIME-Version: 1.0
--- Project2/app/routes.py
+++ Project2/app/routes.py
@@ -96,6 +96,82 @@ def increase_workers():
         ]
     )
 
+    inst_id = []
+    for instance in instances:
+        inst_id.append(instance.id)
+    print('Our worker pool has', len(inst_id), "instances running currently.")
+    if len(inst_id) >= 8:
+        print('Our worker pool is fully loaded with', len(inst_id), "running now.")
+        print('Returning to worker control...')
+        return redirect(url_for('worker_control'))
+
+    instance = ec2.create_instances(ImageId=config.image_id,
+                                    InstanceType='t2.medium',
+                                    KeyName=config.key_pair,
+                                    MinCount=1,
+                                    MaxCount=1,  # add one instance per time
+                                    Monitoring={'Enabled': True},
+                                    Placement={'AvailabilityZone': 'us-east-1d',
+                                               'GroupName': config.worker_group},
+                                    SecurityGroups=[config.security_group],
+                                    UserData=config.user_data,
+                                    TagSpecifications=[
+                                           {
+                                                 'ResourceType': 'instance',
+                                                 'Tags': [
+                                                        {
+                                                            'Key': 'Name',
+                                                            'Value': 'Allen - add worker'
+                                                        }
+                                                         ]
+                                           }
+                                    ],
+                                    IamInstanceProfile={'Arn': config.iam_arn}
+                                    )
+    instance = instance[0]  # only one instance in the instance list
+    print('A new instance ', instance, 'is successfully added.')
+
+    # Wait until the instance runs
+    instance.wait_until_running(
+        Filters=[
+            {
+                'Name': 'instance-id',
+                'Values': [instance.id]
+            },
+        ],
+    )
+    print('The new instance', instance, 'is currently running...')
+
+    elb = boto3.client('elbv2')  # Load Balancer
+    print('A new instance is being registered to the ELB target group...')
+    # Registers the specific targets with the specific target group
+    elb.register_targets(
+        # Register targets with a target group by instance ID
+        TargetGroupArn=config.target_group_arn,  # ARN of the target group
+        Targets=[
+            {
+                'Id': instance.id,
+            },
+        ]
+    )
+
+    # Waiting for register success
+    # Returns an object that can wait for some condition
+    this_waiter = elb.get_waiter('target_in_service')
+    print('AllenXXXXXXX ', this_waiter)
+    #### Comment out cuz all are unhealthy now ####
+  #  this_waiter.wait(
+  #      TargetGroupArn=config.target_group_arn,
+  #      Targets=[
+  #          {
+  #              'Id': instance.id,
+  #          },
+  #      ]
+  #  )
+    print('A new instance ', instance.id, 'is successfully registered.')
+
+    return redirect(url_for('worker_control'))
+
 
 @app.route('/decrease_workers', methods=['GET', 'POST'])
 def decrease_workers():
@@ -121,7 +197,7 @@ def decrease_workers():
     # If there are more than one running instances, remove the oldest one
     if len(inst_id) > 1:
         inst_Id = inst_id[0]  # instance ID of the removing instance
-        print('Deregistering instance-ID:', inst_Id)
+        print('This instance:', inst_Id, 'is being de-registered...')
         elb = boto3.client('elbv2')
         elb.deregister_targets(
             TargetGroupArn=config.target_group_arn,
@@ -132,8 +208,8 @@ def decrease_workers():
             ]
         )
 
-        waiter = elb.get_waiter('target_deregistered')
-        waiter.wait(
+        this_waiter = elb.get_waiter('target_deregistered')
+        this_waiter.wait(
             TargetGroupArn=config.target_group_arn,
             Targets=[
                 {
@@ -141,9 +217,9 @@ def decrease_workers():
                 },
             ],
         )
-        print('Instance-ID: ', inst_Id, 'is deregistered!')
+        print('This instance:', inst_Id, 'has been de-registered.')
 
-        print('Terminating instance-ID:', inst_Id)
+        print('We are terminating instance:', inst_Id, '...')
         # Check whether the instance can be found by its ID
         instance = ec2.instances.filter(InstanceIds=[inst_Id])
         if instance is not None:
@@ -158,6 +234,80 @@ def decrease_workers():
                         },
                     ],
                 )
-                print('Instance-ID: ', inst.id, ' is terminated')
+                print('This instance:', inst.id, 'has been successfully terminated.')
 
+        instances_new = ec2.instances.filter(
+            Filters=[
+
+                {'Name': 'placement-group-name',
+                 'Values': [config.worker_group]},
+
+                {'Name': 'instance-state-name',
+                 'Values': ['running']},
+
+                {'Name': 'image-id',
+                 'Values': [config.image_id]},
+            ]
+        )
+        inst_id_new = []
+        for instance in instances_new:
+            inst_id_new.append(instance.id)
+        print('Our worker pool has', len(inst_id_final), "instances running currently.")
     return redirect(url_for('worker_control'))
+
+
+@app.before_first_request
+# Automatically check if any instance exists.
+# Resize the worker pool size to 1
+# If no instance exists, create one
+# If more than one instance, delete to one
+def auto_check():
+    print('Initiating first check...')
+    ec2 = boto3.resource('ec2')
+    instances = ec2.instances.filter(
+            Filters=[
+
+                {'Name': 'placement-group-name',
+                 'Values': [config.worker_group]},
+
+                {'Name': 'instance-state-name',
+                 'Values': ['running']},
+
+                {'Name': 'image-id',
+                 'Values': [config.image_id]},
+            ]
+        )
+    inst_id = []
+    for instance in instances:
+        inst_id.append(instance.id)
+
+    if (len(inst_id)>1):
+        print('We currently have {} instance running, will shrink to 1...'.format(len(inst_id)))
+        for i in range(1, len(inst_id)):
+            manager.instance_remove(inst_id[i])
+            print('This instance: {} has been removed successfully.'.format(inst_id[i]))
+
+        instances_final = ec2.instances.filter(
+            Filters=[
+
+                {'Name': 'placement-group-name',
+                 'Values': [config.worker_group]},
+
+                {'Name': 'instance-state-name',
+                 'Values': ['running']},
+
+                {'Name': 'image-id',
+                 'Values': [config.image_id]},
+            ]
+        )
+        inst_id_final = []
+        for instance in instances_final:
+            inst_id_final.append(instance.id)
+        print('Our worker pool has', len(inst_id_final), "instances running currently, initiating done.")
+
+    elif (inst_id == []):
+        print('No running instances exist, we are create an instance...')
+        #manager.inst_add()
+
+    else:
+        print('Our worker pool has 1 instance running currently, initiating done.')
