5fa9c940c56edae14e0c0be205c5e4bc6d39a55e
Irene Huang <fulin.huang@mail.utoronto.ca>
2022-03-02 17:23:30 -0500

    Update update_credentials.py

--- Project2(userapp)/update_credentials.py
+++ Project2(userapp)/update_credentials.py
@@ -8,7 +8,7 @@ output_name = "credentials"
 #with open ('metadata.json') as json_file:
 #    data = json.load(json_file)
 
-r = requests.get('http://169.254.169.254/latest/meta-data/iam/security-credentials/Manager_Role_Allen')
+r = requests.get('somepath')
 data = r.json()
 
 access_key_id = data["AccessKeyId"]
@@ -21,7 +21,4 @@ with open(os. path. join(output_path, output_name),"w+") as output_file:
     output_file.write('aws_secret_access_key='+secret_access_key+'\n')
     output_file.write('aws_session_token='+token+'\n')
 print('AWS credential is updated.')
-# [default]
-# aws_access_key_id=ASIAXEBISTHMVLRT42PQ
-# aws_secret_access_key=Fqm41YCJ632TTuiX+HbwJzVuQSS5Shp+rQWX7gSP
-# aws_session_token=FwoGZXIvYXdzEBYaDISVQVdrMF
+
